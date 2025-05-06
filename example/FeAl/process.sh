70afedea56080d3f62d43703e8fe9c8e4e99c059
Yihang Peng <ypeng0082@gmail.com>
2025-05-05 23:24:58 -0400

    Update process.sh

--- example/FeAl/process.sh
+++ example/FeAl/process.sh
@@ -3,6 +3,7 @@
 # Run SQS to get the input file:
 # corrdump -l=conf.in -ro -noe -nop -clus -2=5.0; getclus 
 # mcsqs -l=conf.in -sig=9 -n=xxx -rc -sd=xxx
-# cellcvrt -f -sig=9 < bestsqs.out > conf.vasp
+# cellcvrt -f -sig=9 < bestsqs.out > conf.sqs
+# Then convert conf.sqs into POSCAR format
 
 python3 ../../src/intro_ferric.py -s -rs 20250505
