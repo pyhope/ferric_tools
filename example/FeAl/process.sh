1062dca144c4c44c24d3122f0529442d4f73210e
Yihang Peng <ypeng0082@gmail.com>
2025-05-05 23:23:35 -0400

    Update process.sh

--- example/FeAl/process.sh
+++ example/FeAl/process.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Run SQS to get the input file:
+# corrdump -l=conf.in -ro -noe -nop -clus -2=5.0; getclus 
+# mcsqs -l=conf.in -sig=9 -n=xxx -rc -sd=xxx
+# cellcvrt -f -sig=9 < bestsqs.out > conf.vasp
+
 python3 ../../src/intro_ferric.py -s -rs 20250505
